a1b26ec03b25301519a278b6a93deb1362530969
kazunorikimura <kazunori.p@hotmail.co.jp>
2025-09-24 22:24:41 +0200

    Remove large files from history

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall description:
+## These functions cache the inverse of a matrix to avoid
+## redundant computations, which can be costly for large matrices.
 
-## Write a short comment describing this function
+
+## Creates a special "matrix" object that can store a matrix and its inverse.
+## Provides functions to:
+## 1. set(): update the matrix
+## 2. get(): retrieve the matrix
+## 3. setinv(): cache the inverse
+## 4. getinv(): get the cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## Computes the inverse of the special "matrix" object.
+## If already cached, returns the cached inverse. Otherwise, computes,
+## caches, and returns the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()　
+  if(!is.null(i)) { 
+    message("getting cached data")  　
+    return(i)
+  }
+  data <- x$get() 
+  i <- solve(data, ...)
+  x$setinv(i)　
+  i  
 }
+
+
+
